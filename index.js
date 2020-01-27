f9bd132989a0cbb919bc19590a65b220ad35fab5
Samar Omar <samaromm98@gmail.com>
2020-01-27 17:24:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Write your solution in this file!
 let driver={}
 
-function updateDriverWithKeyAndValue(driver,key,value){
-  let newDriv={...driver}
-  newDriv.key=value
-  return newDriv
+function updateDriverWithKeyAndValue(obj,key,value){
+ const newObj = { ...obj };
+  newObj.key=value;
+  return newObj
+
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
