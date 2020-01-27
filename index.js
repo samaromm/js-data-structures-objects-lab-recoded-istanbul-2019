52cabcb549127b0d8cc0d4f1f1054f877e146fb6
Samar Omar <samaromm98@gmail.com>
2020-01-27 17:43:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ return Object.assign({},driver,{[key]:value})
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
-  driver[key]=value
+  driver.key=value
   return driver
 }
 
